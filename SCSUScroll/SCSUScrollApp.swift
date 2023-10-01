fae3171e01d1ad8ed458a35c88b0a42cbbf1f978
Andrii Karpenko <ximerg002@gmail.com>
2023-10-01 22:09:30 +0200

    Initial Commit

---
+++ SCSUScroll/SCSUScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCSUScrollApp.swift
+//  SCSUScroll
+//
+//  Created by Andrey Karpenko  on 01.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SCSUScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
