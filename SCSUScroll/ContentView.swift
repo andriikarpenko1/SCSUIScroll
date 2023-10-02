1e6968de0323d5a158b28a6bfbb35420b01fbb52
Andrii Karpenko <ximerg002@gmail.com>
2023-10-02 10:40:05 +0200

    Added full screen scroll with adjustable content margins(bottom)

--- SCSUScroll/ContentView.swift
+++ SCSUScroll/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    let bottomViewHeight: CGFloat = 50.0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            ZStack(alignment: .bottom) {
+                ScrollableView(bottomViewHeight: bottomViewHeight)
+                Color.red.frame(height: bottomViewHeight).opacity(0.5)
+            }
+            .tabItem { Label("Single Tab", systemImage: "scroll.fill") }
         }
-        .padding()
+    }
+}
+
+struct ScrollableView: View {
+    var bottomViewHeight: CGFloat = 50.0
+
+    var body: some View {
+        ScrollView {
+            ForEach(0..<100) { i in
+                Text("\(i)")
+                    .frame(maxWidth: .infinity)
+            }
+        }.contentMargins(.bottom, bottomViewHeight)
     }
 }
 
